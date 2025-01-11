9101ec2b2688e6fadfdd5d630f849823d48c1d5a
CodingMiso <irene.lee0905@gmail.com>
2025-01-11 12:55:31 +1100

    test commit again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docss
+            Read our docsss
           </a>
         </div>
       </main>
